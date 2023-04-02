019d4ffdca6b031d64fcf253d8ea02cda0a92d3b
Syconn <Syconngaming@gmail.com>
2023-04-02 19:46:59 -0400

    Initial commit

---
+++ src/main/java/syconn/swe/Main.java
@@ -0,0 +1,54 @@
+package syconn.swe;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+import syconn.swe.init.ModItems;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Main.MODID)
+public class Main {
+
+    public static final String MODID = "swe";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        ModItems.BLOCKS.register(modEventBus);
+        ModItems.ITEMS.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
---
+++ src/main/java/syconn/swe/init/ModItems.java
@@ -0,0 +1,38 @@
+package syconn.swe.init;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import javax.annotation.Nullable;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static syconn.swe.Main.MODID;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+
+
+
+    private static <T extends Block> RegistryObject<T> register(String id, Supplier<T> blockSupplier)
+    {
+        return register(id, blockSupplier, block1 -> new BlockItem(block1, new Item.Properties()));
+    }
+
+    private static <T extends Block> RegistryObject<T> register(String id, Supplier<T> blockSupplier, @Nullable Function<T, BlockItem> supplier)
+    {
+        RegistryObject<T> registryObject = BLOCKS.register(id, blockSupplier);
+        if(supplier != null)
+        {
+            ITEMS.register(id, () -> supplier.apply(registryObject.get()));
+        }
+        return registryObject;
+    }
+}
